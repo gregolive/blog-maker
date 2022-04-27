1769da085244c252ec9b2ca526e5507acb4dc3d4
Gregory Olive <gregoryolive@outlook.com>
2022-04-27 15:09:38 +0900

    Update post creation

--- src/components/post-form/PostForm.js
+++ src/components/post-form/PostForm.js
@@ -1,24 +1,44 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Editor } from '@tinymce/tinymce-react';
+import axios from 'axios';
 import './PostForm.css';
 
 const PostForm = () => {
-  const [contentEditor, setContentEditor] = useState();
-  const handleEditorChange = (content) => setContentEditor(content);
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [preview, setPreview] = useState('');
+  const [visibility, setVisibility] = useState('Visible');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const apiURL = 'http://localhost:3001/api/v1/post/create';
+    axios.post(apiURL, {
+      title,
+      content,
+      preview,
+      visibility,
+    }).then((res) => {
+      navigate(`/post/${res.data}`);
+    }, (err) => {
+      console.log(err);
+    });
+  };
 
   return (
     <main>
       <h1>New blog post</h1>
 
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <fieldset>
-          <label htmlFor='title'>Title</label>
-          <input type='text' name='title' id='title' className='dynamic-input' />
+          <label htmlFor='title' className='required-field'>Title</label>
+          <input type='text' name='title' id='title' className='dynamic-input' maxLength='100' value={title} onChange={(e) => setTitle(e.target.value)} />
         </fieldset>
 
         <fieldset>
-          <label htmlFor='content'>Content</label>
+          <label htmlFor='content' className='required-field'>Content</label>
           <Editor
             apiKey={process.env.REACT_APP_TINYMCE}
             init={{
@@ -31,21 +51,21 @@ const PostForm = () => {
               content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
             }}
             id='content'
-            value={contentEditor}
-            onEditorChange={handleEditorChange}
+            value={content}
+            onEditorChange={(e) => setContent(e)}
           />
         </fieldset>
 
         <fieldset>
           <label htmlFor='preview'>Preview</label>
-          <textarea name='preview' id='preview' rows='4' />
+          <textarea name='preview' id='preview' className='dynamic-input' rows='5' maxLength='200' value={preview} onChange={(e) => setPreview(e.target.value)} />
         </fieldset>
 
         <fieldset>
-          <label htmlFor='visibility'>Visibility</label>
-          <select name='visibility' id='visibility' className='dynamic-input'>
-            <option value="visible">Visible</option>
-            <option value="hidden">Hidden</option>
+          <label htmlFor='visibility' className='required-field'>Visibility</label>
+          <select name='visibility' id='visibility' className='dynamic-input' value={visibility} onChange={(e) => setVisibility(e.target.value)} >
+            <option value="Visible">Visible</option>
+            <option value="Hidden">Hidden</option>
           </select>
         </fieldset>
 
