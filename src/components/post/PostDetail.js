9c318b0e56fa2c6af19480f969f833c31188bef7
Gregory Olive <gregoryolive@outlook.com>
2022-05-07 16:07:16 +0900

    Implement comment editing

--- src/components/post/PostDetail.js
+++ src/components/post/PostDetail.js
@@ -18,7 +18,8 @@ const PostForm = () => {
   const [newComment, setNewComment] = useState('');
   const [comments, setComments] = useState([]);
   const [commentMsg, setCommentMsg] = useState(null);
-  const [markDelete, setMarkDelete] = useState(false);
+  const [markDelete, setMarkDelete] = useState(null);
+  const [markEdit, setMarkEdit] = useState(null);
 
   // Fetch post data
   useEffect(() => {
@@ -34,7 +35,7 @@ const PostForm = () => {
   }, [postTitle]);
 
   // Create new comment
-  const formSubmit = () => {
+  const createFormSubmit = () => {
     const apiURL = `http://localhost:3001/api/v1/post/${post._id}/comments/create`;
 
     axios.post(apiURL, {
@@ -46,9 +47,9 @@ const PostForm = () => {
     }, (err) => setServerError(err));
   };
 
-  const handleSubmit = (e) => {
+  const handleCreateSubmit = (e) => {
     e.preventDefault();
-    if (newComment !== '') { formSubmit(); }
+    if (newComment !== '') { createFormSubmit(); }
   };
 
   // Delete comment
@@ -58,8 +59,30 @@ const PostForm = () => {
     axios.post(apiURL).then((res) => {
       setComments(comments.filter((comment) => comment !== markDelete));
       setCommentMsg(res.data.msg);
-      setMarkDelete(false);
+      setMarkDelete(null);
     }, (err) => setServerError(err));
+  };
+
+  // Edit comment
+  const editFormSubmit = () => {
+    const apiURL = `http://localhost:3001/api/v1/post/${post._id}/comments/${markEdit._id}/update`;
+
+    axios.post(apiURL, {
+      content: markEdit.content,
+      user_id: user._id,
+    }).then((res) => {
+      setComments(comments.map((comment) => {
+        if (comment._id !== markEdit._id) { return comment; }
+        return res.data.comment;
+      }));
+      setCommentMsg(res.data.msg);
+      setMarkEdit(null);
+    }, (err) => setServerError(err));
+  };
+
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+    editFormSubmit();
   };
 
   return ((Object.keys(post).length > 0) ? (
@@ -106,34 +129,47 @@ const PostForm = () => {
             <h2>Comments <span className='CommentCount'>{comments.length}</span></h2>
 
             <div className='CommentGrid'>
-              <form className='CommentForm' onSubmit={(e) => handleSubmit(e)}>
+              <form className='CommentForm' onSubmit={(e) => handleCreateSubmit(e)}>
                 <fieldset>
                   <label htmlFor='comment' />
                   <input type='text' id='comment' name='comment' placeholder='Add comment' value={newComment} onChange={(e) => setNewComment(e.target.value)} className='CommentInput' />
                 </fieldset>
                 <button type='submit' className='Btn InlineBtn'>
-                <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='20' height='20' viewBox='0 0 24 24' fill='currentColor'><path d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' /></svg>
+                  <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='20' height='20' viewBox='0 0 24 24' fill='currentColor'><path d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' /></svg>
                 </button>
               </form>
 
               {comments.map((comment, index) =>
-                <div className='Comment' key={index}>
-                  <strong>{comment.author.username}</strong>
-                  <p>{comment.content}</p>
-                  <small>
-                  {format(parseISO(comment.created_at), ' MMMM dd, yyyy hh:mm aa')}
-                    {(user._id === comment.author._id) ? (
-                      <div className='CommentButtons'>
-                        <button type='button' className='Btn'>
-                          <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='18' height='18' viewBox='0 0 24 24' fill='currentColor'><path d='M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z' /></svg>
-                        </button>
-                        <button type='button' className='Btn' onClick={() => setMarkDelete(comment)}>
-                          <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='18' height='18' viewBox='0 0 24 24' fill='currentColor'><path d='M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z' /></svg>
-                        </button>
-                      </div>
-                    ) : null}
-                  </small>
-                </div>
+                (markEdit && comment._id === markEdit._id) ? (
+                  <form className='EditCommentForm' onSubmit={(e) => handleEditSubmit(e)} key={index}>
+                    <fieldset>
+                      <label htmlFor='comment' />
+                      <input type='text' id='comment' name='comment' placeholder='Add comment' value={markEdit.content} onChange={(e) => setMarkEdit({...markEdit, content: e.target.value})} className='CommentInput' />
+                    </fieldset>
+                    <div className='CommentEditBtns'>
+                      <button type='submit' className='Btn'>Save</button>
+                      <button type='button' className='Btn' onClick={() => setMarkEdit(null)}>Cancel</button>
+                    </div>
+                  </form>
+                ) : (
+                  <div className='Comment' key={index}>
+                    <strong>{comment.author.username}</strong>
+                    <p>{comment.content}</p>
+                    <small>
+                      {format(parseISO(comment.created_at), ' MMMM dd, yyyy hh:mm aa')}
+                      {(user._id === comment.author._id) ? (
+                        <div className='CommentButtons'>
+                          <button type='button' className='Btn' onClick={() => setMarkEdit(comment)}>
+                            <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='18' height='18' viewBox='0 0 24 24' fill='currentColor'><path d='M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z' /></svg>
+                          </button>
+                          <button type='button' className='Btn' onClick={() => setMarkDelete(comment)}>
+                            <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='18' height='18' viewBox='0 0 24 24' fill='currentColor'><path d='M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z' /></svg>
+                          </button>
+                        </div>
+                      ) : null}
+                    </small>
+                  </div>
+                )
               )}
             </div>
           </section>
